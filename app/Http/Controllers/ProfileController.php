675b117b6473934735fb33ad9e8e8db493e508d0
Marks Polakovs <marks@markspolakovs.me>
2017-07-21 21:22:52 +0300

    fix(profile/propositions): non-localised date string

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -113,6 +113,7 @@ class ProfileController extends Controller
     
     public function propositions() {
     	$user = Auth::user();
+		Carbon::setLocale($user->language());
     	$propositionFactory = new PropositionFactory();
     	$propositionsCount = $propositionFactory->getPropositionsCountByUser($user->userId());
     	$viewUser = [
